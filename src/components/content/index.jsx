e51c5a2ca1cd5cd3d98172784fd277ec6165874b
PREDAtorul <74401212+PREDAtorul@users.noreply.github.com>
2022-08-10 18:16:10 +0300

    test4

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,180 +1,18 @@
 import React from "react";
 import "antd/dist/antd.css";
-import "./index.css";
+import "./content.css";
 import { Card } from "antd";
-import { Breadcrumb, Layout, Menu } from "antd";
+import { Breadcrumb} from "antd";
 import { Col, Row } from "antd";
 import { Pagination } from 'antd';
 import { Cascader } from 'antd';
-import { TeamOutlined, CalendarOutlined,SettingOutlined, BgColorsOutlined, FireOutlined, RotateLeftOutlined} from '@ant-design/icons';
+import { TeamOutlined, CalendarOutlined,SettingOutlined, BgColorsOutlined, FireOutlined} from '@ant-design/icons';
 
 
-const { Header, Content, Footer } = Layout;
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-const optionslocation = [
-  {
-    value: 'Romania',
-    label: 'Romania',
-    children: [
-      {
-        value: 'Brasov',
-        label: 'Brasov',
-        
-      },
-      {
-        value: 'Bucuresti',
-        label: 'Bucuresti',
-        
-      },
-      {
-        value: 'Timisoara',
-        label: 'Timisoara',
-        
-      },
-      {
-        value: 'Cluj',
-        label: 'Cluj',
-        
-      },
-    ],
-  },
-  
-];
-const optionsbrand = [
-  {
-    value: 'Dacia',
-    label: 'Dacia',
-    children: [
-      {
-        value: 'Logan',
-        label: 'Logan',
-        
-      },
-      {
-        value: 'Sandero',
-        label: 'Sandero',
-        
-      },
-      {
-        value: 'Duster',
-        label: 'Duster',
-        
-      },
-      {
-        value: 'Spring',
-        label: 'Spring',
-        
-      },
-    ],
-  },
-  {
-    value: 'Volkswagen',
-    label: 'Volkswagen',
-    children: [
-      {
-        value: 'Passat',
-        label: 'Passat',
-        
-      },
-      {
-        value: 'Golf',
-        label: 'Golf',
-        
-      },
-      {
-        value: 'eUp',
-        label: 'eUp',
-        
-      },
-      {
-        value: 'Polo',
-        label: 'Polo',
-        
-      },
-    ],
-  },
-  {
-    value: 'BMW',
-    label: 'BMW',
-    children: [
-      {
-        value: '1 series',
-        label: '1 series',
-        
-      },
-      {
-        value: '3 series',
-        label: '3 series',
-        
-      },
-      {
-        value: '5 series',
-        label: '5 series',
-        
-      },
-      {
-        value: '7 series',
-        label: '7 series',
-        
-      },
-    ],
-  },
-  {
-    value: 'Audi',
-    label: 'Audi',
-    children: [
-      {
-        value: 'A3',
-        label: 'A3',
-        
-      },
-      {
-        value: 'A4',
-        label: 'A4',
-        
-      },
-      {
-        value: 'A6',
-        label: 'A6',
-        
-      },
-      {
-        value: 'A8',
-        label: 'A8',
-        
-      },
-    ],
-  },
-  
-];
-const onChange = (value) => {
-  console.log(value);
-};
-const items = [
-  getItem('Acasa', '1'),
-  getItem('Masini', '2'),
-  getItem('Profilul meu', '3')
-];
 const App = () => 
 (
-  <Layout className="layout">
-    <Header>
-      <title>Take A Ride</title>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["2"]}
-        items={items}
-      />
-    </Header>
-    <Content
+
+<Content
       style={{
         padding: "0 50px"
       }}
@@ -313,16 +151,5 @@ const App = () =>
       </Row>
       <Pagination align={'center'} defaultCurrent={1} total={50} />
     </Content>
-    
-    <Footer
-      style={{
-        textAlign: "center"
-      }}
-    >
-      Hello
-    </Footer>
-  </Layout>
-); 
-    
-
+);
 export default App;
---
+++ src/containers/locations/locations.jsx
@@ -0,0 +1,29 @@
+export default [
+    {
+      value: 'Romania',
+      label: 'Romania',
+      children: [
+        {
+          value: 'Brasov',
+          label: 'Brasov',
+          
+        },
+        {
+          value: 'Bucuresti',
+          label: 'Bucuresti',
+          
+        },
+        {
+          value: 'Timisoara',
+          label: 'Timisoara',
+          
+        },
+        {
+          value: 'Cluj',
+          label: 'Cluj',
+          
+        },
+      ],
+    },
+    
+  ];
