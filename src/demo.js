76f6948bd3ab39f2404daa0ea19a9403b49587fa
PREDAtorul <74401212+PREDAtorul@users.noreply.github.com>
2022-08-10 15:02:38 +0300

    test2

--- src/demo.js
+++ src/demo.js
@@ -1,18 +1,13 @@
-import React, { useState } from 'react';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Card } from 'antd';
-import {
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  CarOutlined,
-} from '@ant-design/icons';
-import Logo from './img/logo.jpg';
-import { Breadcrumb, Layout, Menu } from 'antd';
+import React from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Card } from "antd";
+import { Breadcrumb, Layout, Menu } from "antd";
+import { Col, Row } from "antd";
+import { Pagination } from 'antd';
+import { Cascader } from 'antd';
 
-const { Header, Content, Footer, Sider } = Layout;
-
+const { Header, Content, Footer } = Layout;
 function getItem(label, key, icon, children) {
   return {
     key,
@@ -21,84 +16,296 @@ function getItem(label, key, icon, children) {
     label,
   };
 }
-
-const items = [
-  getItem('Acasa', '1', <PieChartOutlined />),
-  getItem('Masini', 'sub1', <CarOutlined />, [
-    getItem('Volkswagen', '3'),
-    getItem('BMW', '4'),
-    getItem('Mercedes', '5'),
-    getItem('Audi', '6'),
-    getItem('Opel', '7'),
-    getItem('Toyota','8'),
-    getItem('Honda','9'),
-    getItem('Dacia','10'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '11'), getItem('Team 2', '12')]),
-  getItem('Files', '13', <FileOutlined />),
+const optionslocation = [
+  {
+    value: 'Romania',
+    label: 'Romania',
+    children: [
+      {
+        value: 'Brasov',
+        label: 'Brasov',
+        
+      },
+      {
+        value: 'Bucuresti',
+        label: 'Bucuresti',
+        
+      },
+      {
+        value: 'Timisoara',
+        label: 'Timisoara',
+        
+      },
+      {
+        value: 'Cluj',
+        label: 'Cluj',
+        
+      },
+    ],
+  },
+  
 ];
-
-const App = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  return (
-    <Layout
+const optionsbrand = [
+  {
+    value: 'Dacia',
+    label: 'Dacia',
+    children: [
+      {
+        value: 'Logan',
+        label: 'Logan',
+        
+      },
+      {
+        value: 'Sandero',
+        label: 'Sandero',
+        
+      },
+      {
+        value: 'Duster',
+        label: 'Duster',
+        
+      },
+      {
+        value: 'Spring',
+        label: 'Spring',
+        
+      },
+    ],
+  },
+  {
+    value: 'Volkswagen',
+    label: 'Volkswagen',
+    children: [
+      {
+        value: 'Passat',
+        label: 'Passat',
+        
+      },
+      {
+        value: 'Golf',
+        label: 'Golf',
+        
+      },
+      {
+        value: 'eUp',
+        label: 'eUp',
+        
+      },
+      {
+        value: 'Polo',
+        label: 'Polo',
+        
+      },
+    ],
+  },
+  {
+    value: 'BMW',
+    label: 'BMW',
+    children: [
+      {
+        value: '1 series',
+        label: '1 series',
+        
+      },
+      {
+        value: '3 series',
+        label: '3 series',
+        
+      },
+      {
+        value: '5 series',
+        label: '5 series',
+        
+      },
+      {
+        value: '7 series',
+        label: '7 series',
+        
+      },
+    ],
+  },
+  {
+    value: 'Audi',
+    label: 'Audi',
+    children: [
+      {
+        value: 'A3',
+        label: 'A3',
+        
+      },
+      {
+        value: 'A4',
+        label: 'A4',
+        
+      },
+      {
+        value: 'A6',
+        label: 'A6',
+        
+      },
+      {
+        value: 'A8',
+        label: 'A8',
+        
+      },
+    ],
+  },
+  
+];
+const onChange = (value) => {
+  console.log(value);
+};
+const items = [
+  getItem('Acasa', '1'),
+  getItem('Masini', '2'),
+  getItem('Profilul meu', '3')
+];
+const App = () => 
+(
+  <Layout className="layout">
+    <Header>
+      
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={["2"]}
+        items={items}
+      />
+    </Header>
+    <Content
       style={{
-        minHeight: '100vh',
+        padding: "0 50px"
       }}
     >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="logo"> <img src={Logo} alt="Car Rental" style={{width:167, height:40}}/> </div>
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout className="site-layout">
-        <Header
-          className="site-layout-background"
-          style={{
-            padding: 0,
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
-          }}
-        >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
-            <Breadcrumb.Item>Masini</Breadcrumb.Item>
-            <Breadcrumb.Item>Volkswagen</Breadcrumb.Item>
-          </Breadcrumb>
+
+      
+      <Breadcrumb
+        style={{
+          margin: "16px 0"
+        }}
+      >
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Masini</Breadcrumb.Item>
+        <Breadcrumb.Item>Volkswagen</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-content">Content</div>
+      <Cascader size="large" options={optionslocation} onChange={onChange} />
+      <Cascader size="large" options={optionsbrand} onChange={onChange} />
+    
+      <br/>
+      <br/>
+      <Row gutter={[16, 24]} justify="space-evenly">
+        <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4}>
           <Card
-            cover=
-              {
-                <img 
-                  src={require('./img/passat.png')}
-                  alt="passat" 
-                 />
-              }
+            cover={<img src={require("./img/passat.png")} alt="passat" />}
             title="Volkswagen Passat"
             extra={<a href="#">Detalii</a>}
             style={{
-            width: 300,
-                    }}
-            >
-      <p>Combustibil: Diesel</p>
-      <p>Culoare: Gri</p>
-      <p>Consum mediu: 5 l/100km</p>
-    </Card>
-        </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
-  );
-};
+              width: 250
+            }}
+          >
+            <p>Combustibil: Diesel</p>
+            <p>Culoare: Gri</p>
+            <p>Consum mediu: 5 l/100km</p>
+          </Card>
+        </Col>
+        <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4}>
+          <Card
+            cover={<img src={require("./img/passat.png")} alt="passat" />}
+            title="Volkswagen Passat"
+            extra={<a href="#">Detalii</a>}
+            style={{
+              width: 250
+            }}
+          >
+            <p>Combustibil: Diesel</p>
+            <p>Culoare: Gri</p>
+            <p>Consum mediu: 5 l/100km</p>
+          </Card>
+        </Col>
+        <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4}>
+          <Card
+            cover={<img src={require("./img/passat.png")} alt="passat" />}
+            title="Volkswagen Passat"
+            extra={<a href="#">Detalii</a>}
+            style={{
+              width: 250
+            }}
+          >
+            <p>Combustibil: Diesel</p>
+            <p>Culoare: Gri</p>
+            <p>Consum mediu: 5 l/100km</p>
+          </Card>
+        </Col>
+        <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4}>
+          <Card
+            cover={<img src={require("./img/passat.png")} alt="passat" />}
+            title="Volkswagen Passat"
+            extra={<a href="#">Detalii</a>}
+            style={{
+              width: 250
+            }}
+          >
+            <p>Combustibil: Diesel</p>
+            <p>Culoare: Gri</p>
+            <p>Consum mediu: 5 l/100km</p>
+          </Card>
+        </Col>
+        <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4}>
+          <Card
+            cover={<img src={require("./img/passat.png")} alt="passat" />}
+            title="Volkswagen Passat"
+            extra={<a href="#">Detalii</a>}
+            style={{
+              width: 250
+            }}
+          >
+            <p>Combustibil: Diesel</p>
+            <p>Culoare: Gri</p>
+            <p>Consum mediu: 5 l/100km</p>
+          </Card>
+        </Col>
+        <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4}>
+          <Card
+            cover={<img src={require("./img/passat.png")} alt="passat" />}
+            title="Volkswagen Passat"
+            extra={<a href="#">Detalii</a>}
+            style={{
+              width: 250
+            }}
+          >
+            <p>Combustibil: Diesel</p>
+            <p>Culoare: Gri</p>
+            <p>Consum mediu: 5 l/100km</p>
+          </Card>
+        </Col>
+        <Col xs={20} sm={16} md={12} lg={8} xl={6} xxl={4}>
+          <Card
+            cover={<img src={require("./img/passat.png")} alt="passat" />}
+            title="Volkswagen Passat"
+            extra={<a href="#">Detalii</a>}
+            style={{
+              width: 250
+            }}
+          >
+            <p>Combustibil: Diesel</p>
+            <p>Culoare: Gri</p>
+            <p>Consum mediu: 5 l/100km</p>
+          </Card>
+        </Col>
+      </Row>
+      <Pagination align={'center'} defaultCurrent={1} total={500} />
+    </Content>
+    
+    <Footer
+      style={{
+        textAlign: "center"
+      }}
+    >
+      Ant Design ©2018 Created by Ant UED
+    </Footer>
+  </Layout>
+); 
+    
 
 export default App;
